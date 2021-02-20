11fdf0843ec72ba6c7812d235594f0a79d1b5e46
Ricardo Constantino <wiiaboo@gmail.com>
2021-02-20 20:28:41 +0000

    commands/help: refactor

--- commands/general/help.js
+++ commands/general/help.js
@@ -1,44 +1,67 @@
 const { embedMessage } = require("../../libs/general/embedMessage");
 const { PREFIX, IRUMIN_CHA } = require("../../config/config.json");
+const { Message } = require('discord.js');
 
+/**
+ * @typedef Command
+ * @property {string} name - Command Name
+ * @property {string[]} aliases - Aliases to Command, can be empty
+ * @property {'general'|'messages'|'music'} category - Command category
+ * @property {string} description - Command description
+ * @property {Function} execute - Command behavior
+ */
+
+/**
+ * @type Command
+ */
 module.exports = {
   name: "help",
-  aliases: ["h"],
+  aliases: ["h", '?'],
   category: "general",
   description: "",
+  /**
+   * @param {Message} message
+   * @param {string[]} args
+   */
   execute(message, args) {
+    /**
+     * @type {Command[]}
+     */
     let commands = message.client.commands.array();
-    let helpList = [];
+    let subject = Array.isArray(args) && args.length ? args.shift() : null;
+    let command = commands.find(e => e.name === subject || e.aliases.includes(subject));
 
-    if(args.length === 0){
-      helpTitle = ""
-      commands.forEach(function(item) {
-        if (item.name !== "help") {
-          let helpCmd = helpList.find(i => i.name === item.category);
-          
-          if(!helpCmd){
-            let newHelpCmd = {
-              'name': item.category,
-              'value': ["\`" + PREFIX + item.name + "\`"],
-              'inline': false
-            };
-            
-            helpList.push(newHelpCmd);
-          }else{
-            helpCmd.value.push("\`" + PREFIX + item.name + "\`");
-          }
+    let title = 'IRUMIN | HOW TO USE' + (!command ? '' : `[ ${PREFIX+command.name.toUpperCase()} ]`),
+        description = 'Try typing `!help [command]` for more info.',
+        fields = [],
+        image = '',
+        footerText = 'IRUMIN v0.8',
+        footerImage = message.client.user.avatarURL();
+
+
+    if (!subject) {
+      commands.forEach(item => {
+        // ignore itself
+        if (item.name === this.name) return;
+
+        let helpCmd = fields.find(i => i.name === item.category);
+        if (!helpCmd){
+          helpCmd = { name: item.category, value: [], inline: false };
+          fields.push(helpCmd);
         }
+
+        helpCmd.value.push(`\`${PREFIX}${item.name}\``);
       });
 
-      return message.channel.send(embedMessage(`IRUMIN | HOW TO USE`, `Try typing \`!help [with command]\` for more info.`, helpList, ``, `IRUMIN v0.8`, message.client.user.avatarURL()));
-    }else{
-      let command = commands.find(e => e.name === args[0] || e.aliases[0] === args[0]);    
+    } else if (!command) {
+      description = "Eh? I can't find that anywhere... Are you sure that exists or do you need \`!help\`?";
 
-      if(!command)
-        return message.channel.send(embedMessage(`IRUMIN | HOW TO USE`, `Eh? I can't find that anywhere... Are you sure that exists or do you need \`!help\`?`));
-      
+    } else {
       console.log(command.aliases);
-      return message.channel.send(embedMessage(`IRUMIN | HOW TO USE [ ${PREFIX + command.name.toUpperCase()} ]`, command.description, '', command.image, ``, command.aliases.length >= 1 ? `• You can also use [ ${PREFIX + command.aliases} ]` : ''));
+      if (Array.isArray(command.aliases) && command.aliases.length)
+        fields.push({name: 'Aliases', value: command.aliases.map(a => `\`${PREFIX}${a}\``), inline: false});
     }
+
+    return message.channel.send(embedMessage(title, description, fields, image, footerText, footerImage));
   }
 };
