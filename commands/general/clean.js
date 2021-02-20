51fa5633997be50a2023d22f84ca30a94bfe5ee0
Ricardo Constantino <wiiaboo@gmail.com>
2021-02-20 19:20:28 +0000

    commands/clean: fix breakage if missing MANAGE_MESSAGES permission
    Bot can still delete its own messages, but it'll be slower than using a
    bulk delete, as it's going to delete them one by one.

--- commands/general/clean.js
+++ commands/general/clean.js
@@ -1,21 +1,28 @@
 const botMsg = require("../../libs/general/botMessages");
+const discord = require('discord.js')
 
 module.exports = {
   name: "clean",
   aliases: ["cln"],
   category: "general",
-  description: "Clears all my messages",
+  description: "Cleans channel of all my messages",
+  /**
+   * @param {discord.Message} message
+   */
   execute(message) {
-    try {
-      message.channel.messages.fetch({limit: 100}).then(messages => {
-        const botMessages = messages.filter(msg => msg.author.bot);
-        message.channel.bulkDelete(botMessages);
-        message.channel.send(botMsg.messagesDeleted(message.member.id, botMessages.size)).catch(console.error);
-      })
-    } catch (error) {
-      console.log(error);
-    }
-
-    return message.channel.send(botMsg.botError()).catch(console.error);
+    const me = message.client.user;
+    message.channel.messages.fetch({limit: 100})
+    .then(messages => messages.filter(msg => msg.deletable && msg.author.id === me.id))
+    .then(ownMsgs => {
+      if (message.channel.permissionsFor(me).has('MANAGE_MESSAGES'))
+        return message.channel.bulkDelete(ownMsgs)
+      return Promise.all(ownMsgs.map(msg => msg.delete())).then(res => new discord.Collection(res.filter(m => m.deleted)));
+    })
+    .then(deleted => {
+      message.channel.send(botMsg.messagesDeleted(message.member.id, deleted.size)).catch(console.error)
+    }).catch(e => {
+      console.error(e);
+      message.channel.send(botMsg.botError()).catch(console.error)
+    });
   }
 };
