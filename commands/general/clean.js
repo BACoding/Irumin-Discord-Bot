2fc79de51991fefeb5e33d9870c006f111beb3cf
Ricardo Constantino <wiiaboo@gmail.com>
2021-02-24 01:47:22 +0000

    commands/clean: fix deleted count for non-bulk delete

--- commands/general/clean.js
+++ commands/general/clean.js
@@ -10,19 +10,18 @@ module.exports = {
    * @param {discord.Message} message
    */
   execute(message) {
+    /**
+     * @type discord.TextChannel
+     */
+    const channel = message.channel;
     const me = message.client.user;
-    message.channel.messages.fetch({limit: 100})
+    channel.messages.fetch({limit: 100})
     .then(messages => messages.filter(msg => msg.deletable && msg.author.id === me.id))
     .then(ownMsgs => {
-      if (message.channel.permissionsFor(me).has('MANAGE_MESSAGES'))
-        return message.channel.bulkDelete(ownMsgs)
-      return Promise.all(ownMsgs.map(msg => msg.delete())).then(res => new discord.Collection(res.filter(m => m.deleted)));
+      if (channel.permissionsFor(me).has('MANAGE_MESSAGES'))
+        return channel.bulkDelete(ownMsgs).then(d => d.size);
+      return Promise.all(ownMsgs.map(msg => msg.delete())).then(d => d.length);
     })
-    .then(deleted => {
-      message.channel.send(botMsg.messagesDeleted(message.member.id, deleted.size)).catch(console.error)
-    }).catch(e => {
-      console.error(e);
-      message.channel.send(botMsg.botError()).catch(console.error)
-    });
+    .then(deleted => channel.send(botMsg.messagesDeleted(message.member.id, deleted)));
   }
 };
