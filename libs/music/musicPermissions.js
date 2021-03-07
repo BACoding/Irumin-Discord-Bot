8ad55f2743a37864cfd1c0dd11b4e8ce8bbcd8d8
Ricardo Constantino <wiiaboo@gmail.com>
2021-03-07 16:28:04 +0000

    musicPermissions: do not complain if CHECK_USER_VC is disabled

--- libs/music/musicPermissions.js
+++ libs/music/musicPermissions.js
@@ -1,5 +1,5 @@
 const { embedMessage } = require("../../libs/general/embedMessage");
-const { IRUMIN_TUCC } = require("../../config/config.json");
+const { CHECK_USER_VC } = require("../../config/config.json");
 const { Message } = require("discord.js");
 const { getEmojiOrFallback } = require('../general/emojiUtils');
 
@@ -8,11 +8,11 @@ module.exports = {
    * @param {Message} message
    */
   musicPermissions(message) {
-    const userChannel = message.member.voice?.channelID;
-    const botChannel = message.member.guild.voice?.channelID;
+    const userChannel = message.member?.voice?.channelID;
+    const botChannel = message.member?.guild.voice?.channelID;
 
-    if (userChannel !== botChannel)
-      return message.channel.send(embedMessage(`WHERE ARE YOU?`, `I need you with me <@${member.id}>, together in voice! ${getEmojiOrFallback(message, '😴')}`))
+    if (CHECK_USER_VC && userChannel !== botChannel)
+      return message.channel.send(embedMessage(`WHERE ARE YOU?`, `I need you with me <@${message.member.id}>, together in voice! ${getEmojiOrFallback(message, '😴')}`))
       .catch(console.error);
 
     return true;
